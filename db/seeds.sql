124f24464a203fe57d3af8ba054e7bd0e5b66f00
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2018-03-14 14:40:25 -0500

    Added MySQL schema and seeds.

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO posts 
+(
+  item, 
+  rate, 
+  owner, 
+  location, 
+  category, 
+  desc
+) VALUES (
+  "Fly Rods",
+  "$40/day",
+  "John",
+  "Billings, MT",
+  "Fishing",
+  "I own several fly rods in several weights and lengths. If you're headed out to fish local waters give me a holler!"
+);
+
+INSERT INTO posts
+(
+  item, 
+  rate, 
+  owner, 
+  location, 
+  category,
+  desc
+) VALUES (
+  "Canoes",
+  "$75/day",
+  "Ely, MN",
+  "Boating",
+  "Two great canoes just begging to be used! I promise they won't sink. If you're planning a Boundry Waters trip, look no further!"
+);
+
