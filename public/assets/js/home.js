c11aab61837ca57ced82ba27c0adad59d48f10bf
Your Name <nbos0531@gmail.com>
2018-03-19 23:20:33 -0500

    home page route update

---
+++ public/assets/js/home.js
@@ -0,0 +1,18 @@
+$('#choice').on('click', function(e) {
+	e.preventDefault();
+
+	var choice = $('#user-type').val();
+	console.log(choice);
+
+
+	if (choice == 1) {
+		
+		location.href = "/renter";
+
+		
+	}
+	else if (choice == 2) {
+		location.href = "/owner"
+	}
+
+});
