d0223d2a9ec56ba4c8be16914a162e8a0fe821f5
nbos17 <nbos0531@gmail.com>
2018-03-21 18:18:18 -0500

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,56 +1,56 @@
-INSERT INTO Rentals 
+INSERT INTO Rentals
 (
-  item, 
+  username,
+  item,
   rate, 
   owner, 
   location, 
   category, 
-  imgURL,
   description
 ) VALUES (
+	"john123",
   "Fly Rods",
   "$40/day",
   "John",
   "Billings, MT",
   "Fishing",
-  "/public/assets/css/images/flyfish.jpg",
   "I own several fly rods in several weights and lengths. If you're headed out to fish local waters give me a holler!"
 );
 
 INSERT INTO Rentals
 (
+	username,
   item, 
   rate, 
   owner, 
   location, 
   category,
-  imgURL,
   description
 ) VALUES (
+	"jane123",
   "Canoes",
   "$75/day",
   "Jane",
   "Ely, MN",
   "Boating",
-  "/public/assets/css/images/canoe.jpg",
   "Two great canoes just begging to be used! I promise they won't sink. If you're planning a Boundry Waters trip, look no further!"
 );
 
 INSERT INTO Rentals
 (
+	username,
   item,
   rate,
   owner,
   location,
   category,
-  imgURL,
   description
 ) VALUES (
+	"chad123",
   "Snowboard",
   "$30/day",
   "Chad",
   "Aspen, CO",
   "Skiing/Snowboarding",
-  "/public/assets/css/images/flyfish.jpg",
   "There’s freshies on the super-jib in Aspen, brah. My gear is SICK. Come check it out, gazers!"
 );
