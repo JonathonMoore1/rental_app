ccea2057be3f75b604a6272bc1a6ed13524adfe6
Your Name <nbos0531@gmail.com>
2018-03-21 17:57:23 -0500

    modal/owner page changes

--- config/connection_no_seq.js
+++ config/connection_no_seq.js
@@ -5,7 +5,7 @@ var mysql = require('mysql2');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: "1234",
+  password: "sixhbpH7&",
   port: 3306,
   database: 'rentalApp_db'
 });
